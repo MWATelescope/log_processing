29cfd0b38dd7f61782b660054093325a8f8c9cce
Greg Sleap <greg.sleap@curtin.edu.au>
2022-10-21 17:49:51 +1100

    Initial commit of create table script

---
+++ scripts/create_mwa_job_jobs_history_table.sql
@@ -0,0 +1,91 @@
+--
+-- This script creates a history table for MWA ASVO
+-- v1 and v2 logs to be imported into.
+--
+CREATE TABLE IF NOT EXISTS public.jobs_history
+(
+    job_type integer NOT NULL,
+    job_state integer NOT NULL,
+    user_id bigint NOT NULL,
+    job_params json,
+    error_code integer,
+    error_text text COLLATE pg_catalog."default",
+    created timestamp without time zone DEFAULT now(),
+    started timestamp without time zone,
+    completed timestamp without time zone,
+    product json,
+    id bigint NOT NULL,
+    CONSTRAINT jobs_history_pkey PRIMARY KEY (id),
+    CONSTRAINT jobs_history_user_fkey FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.jobs_history
+    OWNER to mwa_owner;
+-- Index: expiry_index
+
+-- DROP INDEX IF EXISTS public.expiry_index;
+
+CREATE INDEX IF NOT EXISTS expiry_index
+    ON public.jobs_history USING btree
+    (completed ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: fki_jobs_history_user_fkey
+
+-- DROP INDEX IF EXISTS public.fki_jobs_history_user_fkey;
+
+CREATE INDEX IF NOT EXISTS fki_jobs_history_user_fkey
+    ON public.jobs_history USING btree
+    (user_id ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: jobs_history_unique_index
+
+-- DROP INDEX IF EXISTS public.jobs_history_unique_index;
+
+CREATE UNIQUE INDEX IF NOT EXISTS jobs_history_unique_index
+    ON public.jobs_history USING btree
+    (job_type ASC NULLS LAST, (job_params::text) COLLATE pg_catalog."default" ASC NULLS LAST, user_id ASC NULLS LAST)
+    TABLESPACE pg_default
+    WHERE job_state <= 1;
+-- Index: jobs_history_jobid_index
+
+-- DROP INDEX IF EXISTS public.jobs_history_jobid_index;
+
+CREATE INDEX IF NOT EXISTS jobs_history_jobid_index
+    ON public.jobs_history USING btree
+    (id ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: jobs_history_users_idx
+
+-- DROP INDEX IF EXISTS public.jobs_history_users_idx;
+
+CREATE INDEX IF NOT EXISTS jobs_history_users_idx
+    ON public.jobs_history USING btree
+    (user_id ASC NULLS LAST)
+    TABLESPACE pg_default;
+
+-- Trigger: delete_trigger
+
+-- DROP TRIGGER IF EXISTS delete_trigger ON public.jobs_history;
+
+CREATE TRIGGER delete_trigger
+    BEFORE DELETE
+    ON public.jobs_history
+    FOR EACH ROW
+    EXECUTE PROCEDURE public.delete_notify_trigger();
+
+-- Trigger: insert_trigger
+
+-- DROP TRIGGER IF EXISTS insert_trigger ON public.jobs_history;
+
+CREATE TRIGGER insert_trigger
+    AFTER INSERT OR UPDATE 
+    ON public.jobs_history
+    FOR EACH ROW
+    EXECUTE PROCEDURE public.insert_notify_trigger();
